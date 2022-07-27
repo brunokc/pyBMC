212c573dacf501276e1ea54ed713e85526ea888d
Bruno Kraychete da Costa <brunokc@kraychete.com>
2022-07-26 23:36:51 -0700

    Fix API import

--- pyBMC/api.py
+++ pyBMC/api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint #, flash, g, redirect, render_template, request, session, url_for
+
+from flask import Blueprint, request
 from . import Sensors
 
 sensors = Sensors.Sensors()
