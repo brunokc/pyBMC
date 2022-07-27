a6b392c48717ce5b3ff7b7d54c5852c57a9a3761
Bruno Kraychete da Costa <brunokc@kraychete.com>
2022-07-26 23:35:34 -0700

    Move API into its own blueprint

---
+++ pyBMC/api.py
@@ -0,0 +1,90 @@
+from flask import Blueprint #, flash, g, redirect, render_template, request, session, url_for
+from . import Sensors
+
+sensors = Sensors.Sensors()
+
+bp = Blueprint("api", __name__, url_prefix="/api/v1")
+
+def build_fan(index):
+    fan = sensors.case_fans[index]
+    return {
+        "id": fan.id,
+        "name": fan.name,
+        "rpm": fan.rpm,
+        "dutyCycle": fan.duty_cycle
+    }
+
+def build_temp(index):
+    temp = sensors.temp
+    return {
+        "id": index,
+        "name": temp.name,
+        "temperatureC": temp.temperature_c,
+        "humidity": temp.humidity
+    }
+
+def build_psu():
+    return {
+        "powerState": sensors.psu.ps_ok.state
+    }
+
+@bp.route("/state")
+def get_state():
+    sensors.update_state()
+    data = { }
+
+    fan_data = []
+    for i in range(len(sensors.case_fans)):
+        fan_data.append(build_fan(i))
+
+    data.update({
+        "fans": fan_data,
+        "tempSensor": build_temp(0),
+        "psu": build_psu()
+    })
+    return data
+
+@bp.route("/fan/<int:fan_id>", methods=["GET", "PATCH"])
+def fan_state(fan_id):
+    if fan_id < 0 or fan_id >= len(sensors.fans):
+        return "Fan not found", 404
+
+    if request.method == "GET":
+        return build_fan(fan_id)
+
+    elif request.method == "PATCH":
+        data = request.get_json()
+        new_duty_cycle = data["dutyCycle"]
+        if new_duty_cycle < 0 or new_duty_cycle > 100:
+            return "Invalid duty cycle", 400
+
+        fan = sensors.fans[fan_id]
+        fan.set_speed(new_duty_cycle)
+        return "", 204
+
+@bp.route("/temp/<int:temp_id>")
+def temp_state(temp_id):
+    # We only support one for now
+    if temp_id != 0:
+        return "Temp sensor not found", 404
+
+    return {
+        "tempSensor": build_temp(0)
+    }
+
+@bp.route("/psu", methods=["GET", "PATCH"])
+def psu_state():
+    if request.method == "GET":
+        return build_psu()
+
+    elif request.method == "PATCH":
+        data = request.get_json()
+        new_state = data["powerState"]
+        valid_on_values = [1, True, "on"]
+        valid_off_values = [0, False, "off"]
+        if new_state not in valid_on_values and new_state not in valid_off_values:
+            return "Invalid power state", 400
+
+        # Convert to boolean
+        new_state = (new_state in valid_on_values)
+        sensors.psu.power_switch.write(new_state)
--- pyBMC/app.py
+++ pyBMC/app.py
@@ -1,11 +1,8 @@
 
 import os
 
-from flask import Flask, request, render_template
-from . import Sensors
+from flask import Flask, render_template
 from . import version
-
-sensors = Sensors.Sensors()
 
 def create_app(test_config=None):
     app = Flask(__name__.split('.')[0], instance_relative_config=True)
@@ -26,94 +23,14 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    def build_fan(index):
-        fan = sensors.case_fans[index]
-        return {
-            "id": fan.id,
-            "name": fan.name,
-            "rpm": fan.rpm,
-            "dutyCycle": fan.duty_cycle
-        }
-
-    def build_temp(index):
-        temp = sensors.temp
-        return {
-            "id": index,
-            "name": temp.name,
-            "temperatureC": temp.temperature_c,
-            "humidity": temp.humidity
-        }
-
-    def build_psu():
-        return {
-            "powerState": sensors.psu.ps_ok.state
-        }
-
-    @app.route("/api/v1/state")
-    def get_state():
-        sensors.update_state()
-        data = { }
-
-        fan_data = []
-        for i in range(len(sensors.case_fans)):
-            fan_data.append(build_fan(i))
-
-        data.update({
-            "fans": fan_data,
-            "tempSensor": build_temp(0),
-            "psu": build_psu()
-        })
-        return data
-
-    @app.route("/api/v1/fan/<int:fan_id>", methods=["GET", "PATCH"])
-    def fan_state(fan_id):
-        if fan_id < 0 or fan_id >= len(sensors.fans):
-            return "Fan not found", 404
-
-        if request.method == "GET":
-            return build_fan(fan_id)
-
-        elif request.method == "PATCH":
-            data = request.get_json()
-            new_duty_cycle = data["dutyCycle"]
-            if new_duty_cycle < 0 or new_duty_cycle > 100:
-                return "Invalid duty cycle", 400
-
-            fan = sensors.fans[fan_id]
-            fan.set_speed(new_duty_cycle)
-            return "", 204
-
-    @app.route("/api/v1/temp/<int:temp_id>")
-    def temp_state(temp_id):
-        # We only support one for now
-        if temp_id != 0:
-            return "Temp sensor not found", 404
-
-        return {
-            "tempSensor": build_temp(0)
-        }
-
-    @app.route("/api/v1/psu", methods=["GET", "PATCH"])
-    def psu_state():
-        if request.method == "GET":
-            return build_psu()
-
-        elif request.method == "PATCH":
-            data = request.get_json()
-            new_state = data["powerState"]
-            valid_on_values = [1, True, "on"]
-            valid_off_values = [0, False, "off"]
-            if new_state not in valid_on_values and new_state not in valid_off_values:
-                return "Invalid power state", 400
-
-            # Convert to boolean
-            new_state = (new_state in valid_on_values)
-            sensors.psu.power_switch.write(new_state)
+    from . import api
+    app.register_blueprint(api.bp)
 
     @app.route("/")
     def index():
         project_name = __name__.split('.')[0]
         project_version = version.__version__
-        return render_template("main.html", **locals(), sensors = sensors)
+        sensors = api.sensors
+        return render_template("main.html", **locals())
 
     return app
