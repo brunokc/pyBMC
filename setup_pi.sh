3d7196ad8bf80f1a1c3a4df2b51977477df7d25a
Bruno Kraychete da Costa <brunokc@kraychete.com>
2022-08-03 20:49:28 -0700

    Allow specifying a branch as argument

--- setup_pi.sh
+++ setup_pi.sh
@@ -2,11 +2,13 @@
 
 set -x
 
+branch=${1:-main}
+
 echo Downloading pyBMC...
 tmpdir=`mktemp -d`
 curdir=$PWD
 cd $tmpdir
-curl -sL -o pybmc.zip https://github.com/brunokc/pyBMC/archive/main.zip
+curl -sL -o pybmc.zip https://github.com/brunokc/pyBMC/archive/${branch}.zip
 unzip -q pybmc.zip
 mv pyBMC-main ${curdir}/pyBMC
 cd ${curdir}/pyBMC
@@ -19,6 +21,6 @@ echo Creating Python virtual environment and activating it...
 python -m venv venv
 source venv/bin/activate
 
-echo Installing dependencies...
+echo Installing pyBMC dependencies...
 pip install --upgrade pip
 pip install -r requirements.txt
